3a9ea9299a90c94a72ae4764b33132b7819261d7
Praveen <94160698+githubhacker12@users.noreply.github.com>
2024-06-02 16:50:46 +0530

    Add files via upload

---
+++ use.js
@@ -0,0 +1,53 @@
+// Function to toggle dropdown menu
+function setupDropdown(elementSelector, linkSelector, ulSelector) {
+  const element = document.querySelector(elementSelector);
+  const linkIcon = document.querySelector(linkSelector);
+  const ulElement = document.querySelector(ulSelector);
+
+  element.addEventListener("mouseover", () => {
+    linkIcon.className = "bi bi-caret-up";
+    ulElement.style.display = "block";
+  });
+
+  element.addEventListener("mouseout", () => {
+    linkIcon.className = "bi bi-caret-down-fill";
+    ulElement.style.display = "";
+  });
+}
+
+// Setup dropdowns
+setupDropdown(".course", ".course_link i", ".course_ul");
+setupDropdown(".bootcamp", ".bootcamp_link i", ".bootcamp_ul");
+
+// Toggle menu visibility
+const barsEl = document.querySelector(".bars");
+const xmarkEl = document.querySelector(".xmark");
+const linksEl = document.querySelector(".links");
+
+barsEl.addEventListener("click", () => {
+  barsEl.style.display = "none";
+  xmarkEl.style.display = "block";
+  linksEl.style.display = "flex";
+});
+
+xmarkEl.addEventListener("click", () => {
+  xmarkEl.style.display = "none";
+  linksEl.style.display = "none";
+  barsEl.style.display = "block";
+});
+
+// Function to get the current aspect ratio
+function getAspectRatio() {
+  return window.innerWidth / window.innerHeight;
+}
+
+let initialAspectRatio = getAspectRatio();
+
+window.addEventListener("resize", () => {
+  let newAspectRatio = getAspectRatio();
+  let newWidth = window.innerWidth;
+
+  if (newAspectRatio !== initialAspectRatio && newWidth > 900) {
+    window.location.reload();
+  }
+});
