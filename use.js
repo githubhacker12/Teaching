c1c29aa39edf951394c0a4465dd0e20d0a09c736
Praveen <94160698+githubhacker12@users.noreply.github.com>
2024-06-04 13:57:41 +0530

    Update use.js

--- use.js
+++ use.js
@@ -1,45 +1,53 @@
-const select = (selector) => document.querySelector(selector);
-const selectAll = (selector) => document.querySelectorAll(selector);
+// Function to toggle dropdown menu
+function setupDropdown(elementSelector, linkSelector, ulSelector) {
+  const element = document.querySelector(elementSelector);
+  const linkIcon = document.querySelector(linkSelector);
+  const ulElement = document.querySelector(ulSelector);
 
-const addEventListeners = (element, events, handler) => {
-  events.forEach(event => element.addEventListener(event, handler));
-};
-
-const toggleDisplayStyle = (element, displayStyle) => {
-  element.style.display = (element.style.display === displayStyle) ? "" : displayStyle;
-};
-
-const toggleClass = (element, className1, className2) => {
-  element.classList.toggle(className1);
-  if (className2) element.classList.toggle(className2);
-};
-
-const handleHover = (triggerEl, targetEl, className) => {
-  addEventListeners(triggerEl, ['mouseover', 'mouseout'], () => {
-    toggleClass(targetEl, className);
+  element.addEventListener("mouseover", () => {
+    linkIcon.className = "bi bi-caret-up";
+    ulElement.style.display = "block";
   });
-};
 
-const handleToggle = (triggerEl, targetEl, displayStyle) => {
-  addEventListeners(triggerEl, ['click'], () => {
-    toggleDisplayStyle(targetEl, displayStyle);
+  element.addEventListener("mouseout", () => {
+    linkIcon.className = "bi bi-caret-down-fill";
+    ulElement.style.display = "";
   });
-};
+}
 
-handleHover(select('.course'), select('.course_ul'), 'bi-caret-up');
-handleHover(select('.bootcamp'), select('.bootcamp_ul'), 'bi-caret-up');
+// Setup dropdowns
+setupDropdown(".course", ".course_link i", ".course_ul");
+setupDropdown(".bootcamp", ".bootcamp_link i", ".bootcamp_ul");
 
-handleToggle(select('.bars'), select('.links'), 'block');
-handleToggle(select('.xmark'), select('.links'), 'none');
+// Toggle menu visibility
+const barsEl = document.querySelector(".bars");
+const xmarkEl = document.querySelector(".xmark");
+const linksEl = document.querySelector(".links");
 
-addEventListeners(select('.branch'), ['change'], () => {
-  const other_inputEl = select('.other_input');
-  toggleDisplayStyle(other_inputEl, (select('#branch_selector').value === "Other") ? 'flex' : 'none');
+barsEl.addEventListener("click", () => {
+  barsEl.style.display = "none";
+  xmarkEl.style.display = "block";
+  linksEl.style.display = "flex";
 });
 
+xmarkEl.addEventListener("click", () => {
+  xmarkEl.style.display = "none";
+  linksEl.style.display = "none";
+  barsEl.style.display = "block";
+});
+
+// Function to get the current aspect ratio
+function getAspectRatio() {
+  return window.innerWidth / window.innerHeight;
+}
+
+let initialAspectRatio = getAspectRatio();
+
 window.addEventListener("resize", () => {
-  const newAspectRatio = window.innerWidth / window.innerHeight;
-  if (newAspectRatio !== initialAspectRatio && window.innerWidth > 900) {
+  let newAspectRatio = getAspectRatio();
+  let newWidth = window.innerWidth;
+
+  if (newAspectRatio !== initialAspectRatio && newWidth > 900) {
     window.location.reload();
   }
 });
